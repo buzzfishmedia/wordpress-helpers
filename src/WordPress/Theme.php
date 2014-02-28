f1672684c7e1b5b793cbd3a4ee66c2d0447a83be
Frank McCoy <frankmccoy.d@gmail.com>
2014-02-28 14:27:31 -0600

    updated theme

--- src/WordPress/Theme.php
+++ src/WordPress/Theme.php
@@ -2,199 +2,148 @@
 
 namespace WordPress;
 
+/**
+ * Wordpress Theme Class
+ *
+ * @author Frank McCoy <frankmccoy.d@gmail.com>
+ * @version 0.1
+ */
 class Theme
 {
-	public $name;
-	public $setup = array();
-	public $init = array();
-	public $scripts = array();
+    public $theme_name;
+    public $scripts;
 
-	public function __construct($name, $setup = array() )
-	{
-		$this->name = $name;
-		if( ! empty( $setup ) ) {
-			$this->themeSetup($setup);
-		}
+    public function __construct($name, $setup = array() )
+    {
+        $this->theme_name = $name;
+        $this->themeSetup($setup);
+        $this->themeInit();
 
-		add_action('init', array( $this, 'doThemeInit') );
-		add_action('after_setup_theme', array( $this, 'doThemeSetup' ) );
-		add_action('wp_enqueue_scripts', array( $this, 'doThemeScripts' ) );
+    }
+    /**
+     * Setup the Theme here
+     */
+    public function themeSetup($setup = array() )
+    {
 
-	}
+        if( ! empty( $setup ) and $setup != array() ) :
 
-	
+            // Theme Support
+            if ( array_key_exists('theme_support', $setup ) and $setup['theme_support'] != "" ) {
 
-	public function doThemeInit()
-	{
-		$settings = $this->getThemeInit();
+                    $supports = $setup['theme_support'];
 
-		// Content Width
-		if( array_key_exists('content_width', $settings ) and $settings['content_width'] != "" ){
-			if ( ! isset( $content_width ) ){
-				$content_width = $settings['content_width'];
-			} else {
-				$content_width = 640;
-			}
-		}
+                    foreach ($supports as $feature => $value) {
 
-	}
+                        if ( is_array( $value ) and $value != "" ) {
+                            add_theme_support( $feature, $value );
+                        } else {
+                            add_theme_support( $feature );
+                        }
 
-	public function themeInit( $args = array() )
-	{
-		$this->init = $args;
-		return $this->init;
-	}
+                    }
 
-	public function getThemeInit()
-	{
-		return $this->init;
-	}
+            }
 
-	/**
-	 * Theme Setup
-	 */
-	public function doThemeSetup()
-	{
-		$setup = $this->getThemeSetup();
+            register_nav_menu( 'default-menu', 'Default Menu' );
 
-		// Theme Support
-		if ( array_key_exists('theme_support', $setup ) and $setup['theme_support'] != "" ) {
+            // Nav Menus
+            if ( array_key_exists('nav_menus', $setup ) and $setup['nav_menus'] != "" ) {
 
-			$supports = $setup['theme_support'];
+                $navmenus = $setup['nav_menus'];
 
-			foreach ($supports as $feature => $value) {
+                foreach ($navmenus as $menu) {
+                    register_nav_menus( $menu );
+                }
 
-				switch ( $feature ) {
+            }
 
-					case 'automatic-feed-links':
-						add_theme_support( 'automatic-feed-links' );
-						break;
+        endif;
 
-					case 'post-thumbnails':
-						add_theme_support( 'post-thumbnails' );
-						break;
+        add_action( 'after_setup_theme', array( $this, 'themeSetup') );
+    }
 
-					case 'post-formats':
+    public function themeInit()
+    {
 
-						if( $value != "" ) {
-							add_theme_support( 'post-formats', $value );
-						} else {
-							add_theme_support( 'post-formats' );
-						}
-						break;
+    }
 
-					case 'custom-header':
-						add_theme_support( 'custom-header' );
-						break;
+    /**
+     * Helper: wp_enqueue_scripts()
+     *
+     */
+    public function wpEnqueueScripts()
+    {
+        add_action('wp_enqueue_scripts', array( $this, 'enqueueScripts' ) );
+    }
 
-					case 'custom-background':
+    public function addThemeSupport($cb = '')
+    {
+        add_theme_support( $cb );
+    }
 
-						if( $value != "" ) {
-							add_theme_support( 'custom-background', $value );
-						} else {
-							add_theme_support( 'custom-background' );
-						}
-						break;
+    /**
+     * Add scripts to the front-end
+     *
+     */
+    public function enqueueScripts()
+    {
+        $scripts = $this->scripts;
+        
 
-					case 'html5':
-						if( $value != "" ) {
-							add_theme_support( 'html5', $value );
-						} else {
-							add_theme_support( 'html5' );
-						}
-						break;
+        foreach ($scripts as $script) {
 
-					default:
-						# code...
-						break;
-				}
-			}
-		}
+            // Vars
+            $method = isset($script['method']) ? $script['method'] : 'enqueue';
+            $location = isset($script['location']) ? $script['location'] : '';
+            $deps = isset($script['dependencies']) ? $script['dependencies'] : array();
+            $vers = isset($script['version']) ? $script['version'] : false;
+            $admin = isset($script['admin']) ? $script['admin'] : false;
 
-		// Nav Menus
-		if ( array_key_exists('nav_menus', $setup ) and $setup['nav_menus'] != "" ) {
+            // Check Type: Style
+            if ($script['type'] == 'style') {
 
-		    $navmenus = $setup['nav_menus'];
+                // Check Method
+                if ($method == 'register') {
+                    wp_register_style($script['name'], $location, $deps, $vers );
+                } else {
+                    wp_enqueue_style($script['name'], $location, $deps, $vers );
+                }
 
-		    foreach ($navmenus as $menu) {
-		        register_nav_menus( $menu );
-		    }
+            }
+            // Check Type: Script
+            else {
 
-		} else {
-		    register_nav_menu( 'default-menu', 'Default Menu' );
-		}
-	}
+                // Check Method
+                if ($method == 'register') {
+                    wp_register_script($script['name'], $location, $deps, $vers );
+                } else {
+                    wp_enqueue_script( $script['name'], $location, $deps, $vers );
+                }
+            }
 
-	/**
-	 * Set the values of "Setup"
-	 */
-	public function themeSetup($setup)
-	{
-		$this->setup = $setup;
-		return $this->setup;
-	}
+            //$this->wpEnqueueScripts($load);
+        }
 
-	/**
-	 * Get the values of "Setup"
-	*/
-	public function getThemeSetup()
-	{
-		return $this->setup;
-	}
+        //return $this->scripts;
+    }
 
-	public function doThemeScripts()
-	{
-		$scripts = $this->getThemeScripts();
-		
-		foreach ($scripts as $script) {
+    public function addWidgets($widgets)
+    {
+        //print_r($widgets);
+        foreach ($widgets as $widget) {
+            register_sidebar( $widget );
+        }
+    }
 
-			// Vars
-			$method = isset($script['method']) ? $script['method'] : 'enqueue';
-			$location = isset($script['location']) ? $script['location'] : '';
-			$deps = isset($script['dependencies']) ? $script['dependencies'] : array();
-			$vers = isset($script['version']) ? $script['version'] : false;
-			$admin = isset($script['admin']) ? $script['admin'] : false;
+    public function loadScripts($load){
+        $this->addScripts($load);
+        $this->wpEnqueueScripts();
+    }
 
-			// Check Type: Style
-			if ($script['type'] == 'style') {
-
-				// Check Method
-				if ($method == 'register') {
-					wp_register_style($script['name'], $location, $deps, $vers );
-				} else {
-					wp_enqueue_style($script['name'], $location, $deps, $vers );
-				}
-			}
-
-			// Check Type: Script
-			else {
-
-				// Check Method
-				if ($method == 'register') {
-					wp_register_script($script['name'], $location, $deps, $vers );
-				} else {
-					wp_enqueue_script( $script['name'], $location, $deps, $vers );
-				}
-			}
-		}
-
-		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-			wp_enqueue_script( 'comment-reply' );
-		}
-	}
-
-	public function themeScripts( $scripts = array() )
-	{
-		$this->scripts = $scripts;
-		return $this->scripts;
-	}
-
-	public function getThemeScripts()
-	{
-		return $this->scripts;
-	}
-
-
-
+    public function addScripts($scripts){
+        $this->scripts = $scripts;
+        return $this->scripts;
+    }
 
 }
