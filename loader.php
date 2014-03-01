e3f109fd50685c53f94aa8e340263a7900a8003a
Frank McCoy <frankmccoy.d@gmail.com>
2014-02-28 21:41:08 -0600

    mu-plugin type

---
+++ loader.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ Plugin Name: Wordpress Helpers Loader
+ Plugin URI: #
+ Description: Loads the helper plugin file.
+ Author: Frank McCoy
+ Version: 0.0.1
+ Author URI: http://github.com/fmccoy/
+ */
+
+ require_once( WPMU_PLUGIN_DIR . '/wordpress-helpers/wordpress-helpers.php' );
---
+++ wordpress-helpers.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ Plugin Name: Wordpress Helpers
+ Plugin URI: #
+ Description: Adds helper classes used to ease WordPress development.
+ Author: Frank McCoy
+ Version: 0.0.1
+ Author URI: http://github.com/fmccoy/
+ */
+
+ define( 'WPHELPER_DIR', dirname( __FILE__ ) );
